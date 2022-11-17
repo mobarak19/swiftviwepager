535f0d0b29207ef1e916ae27aff2cabc902be21a
mobarak19 <mobarakhossen2012@gmail.com>
2022-11-17 15:14:50 +0600

    localization added

--- SwiftViewPagerImp/FirstVC.swift
+++ SwiftViewPagerImp/FirstVC.swift
@@ -9,13 +9,27 @@ import UIKit
 
 class FirstVC: UIViewController {
 
+    private let myLable:UILabel = {
+        let lbl = UILabel()
+        lbl.text = "Hello".localized()
+        lbl.textAlignment = .center
+        lbl.font = .systemFont(ofSize: 32, weight: .semibold)
+        return lbl
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemTeal
         
+        view.addSubview(myLable)
+        myLable.frame = view.bounds
+        
     }
-    
 
 
+}
+extension String{
+    func localized()->String{
+        return NSLocalizedString(self,tableName: "Localizable",bundle: .main, value: self, comment: self)
+    }
 }
